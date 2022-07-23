62a1abe92f1ab91761826dd7ba9f74c418db3d70
c_stewart27 <cstewart3@student.sjcny.edu>
2022-07-23 14:53:51 -0400

    Initial Commit

---
+++ Learning GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning GitHub
+//
+//  Created by Christopher Stewart on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Learning GitHubUITests/Learning_GitHubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Learning_GitHubUITestsLaunchTests.swift
+//  Learning GitHubUITests
+//
+//  Created by Christopher Stewart on 7/23/22.
+//
+
+import XCTest
+
+class Learning_GitHubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
